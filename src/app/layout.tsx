3f9c315e9b9aa242b5589cbc7ab1d27e6c0c206e
Jay <jaychorat0910@gmail.com>
2025-07-31 16:11:06 +0000

    i want to download  this app and publish it on play store

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 export const metadata: Metadata = {
   title: "Bug Hunters",
   description: "An online detective cyber mystery investigation game.",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -22,6 +23,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Source_Code_Pro:wght@400;700&family=VT323&display=swap"
           rel="stylesheet"
         />
+        <meta name="theme-color" content="#0D110D" />
       </head>
       <body
         className={cn(
