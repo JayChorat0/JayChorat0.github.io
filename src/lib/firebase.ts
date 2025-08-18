3392ecaa46d4265163dd9f0e2ae2f486edbb74da
Jay <jaychorat0910@gmail.com>
2025-08-18 16:09:48 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -5,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "cyber-sleuth-2lw00",
-  appId: "1:218425118218:web:4511fbf82c611feabb99f3",
-  storageBucket: "cyber-sleuth-2lw00.firebasestorage.app",
-  apiKey: "AIzaSyAQ2_9rOMbp3hpoPUJHTGAthzYFjF4FRvo",
-  authDomain: "cyber-sleuth-2lw00.firebaseapp.com",
-  messagingSenderId: "218425118218",
-  measurementId: "G-4PNRXQWJ8P"
+  "projectId": "cyber-sleuth-2lw00",
+  "appId": "1:218425118218:web:4511fbf82c611feabb99f3",
+  "storageBucket": "cyber-sleuth-2lw00.firebasestorage.app",
+  "apiKey": "AIzaSyAQ2_9rOMbp3hpoPUJHTGAthzYFjF4FRvo",
+  "authDomain": "cyber-sleuth-2lw00.firebaseapp.com",
+  "messagingSenderId": "218425118218",
+  "measurementId": "G-4PNRXQWJ8P"
 };
 
 // Initialize Firebase
