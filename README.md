4151528d42e896bda37e8edf8137d2f0769b6efb
Jay <jaychorat0910@gmail.com>
2025-09-17 15:52:55 +0000

    I have hosted code on https://github.com/JayChorat0/JayChorat0.github.io

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This is a Next.js project created in Firebase Studio. It's an online detective cyber mystery investigation game.
 
 This project is deployed on GitHub Pages. You can access the live version here:
-[https://jaychorat0.github.io/BugHunter/](https://jaychorat0.github.io/BugHunter/)
+[https://jaychorat0.github.io/](https://jaychorat0.github.io/)
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 
 import type {NextConfig} from 'next';
 
-const repoName = 'BugHunter'; // IMPORTANT: This is your repository name.
+// For a user/organization site, the repo name is not needed in the path.
+const repoName = ''; 
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  // basePath and assetPrefix should be empty for a user page repository
+  basePath: isProd ? repoName : '',
+  assetPrefix: isProd ? repoName : '',
   typescript: {
     ignoreBuildErrors: true,
   },
